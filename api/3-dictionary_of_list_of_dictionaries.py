79cfebf73d1f19e2d070ce19909a8dcec633cdfb
nimrod02 <letrun.a@gmail.com>
2023-08-22 09:37:38 +0200

    add task 3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Python script that, using a REST API, for a given employee ID.
+"""
+
+import json
+import requests
+
+
+if __name__ == "__main__":
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_id = f"{base_url}/users"
+    todo = f"{base_url}/todos"
+
+    response_user = requests.get(user_id)
+    response_todo = requests.get(todo)
+
+    user_data = response_user.json()
+    todo_data = response_todo.json()
+
+    data_employee = {}
+
+    for users in user_data:
+        user_id = users['id']
+        username = users['username']
+
+        tasks = [{"username": username,
+                  "task": task["title"],
+                  "complete": task["completed"]}
+                 for task in todo_data if task["userId"] == user_id]
+
+        data_employee[user_id] = tasks
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(data_employee, jsonfile)
